dd774cb88b0a237855405585bffd10ff9da25350
unknown <jflg240104@unitec.edu>
2022-08-12 15:48:45 -0600

    avance

--- src/lab4p2_kevinbanegas/Lab4P2_KevinBanegas.java
+++ src/lab4p2_kevinbanegas/Lab4P2_KevinBanegas.java
@@ -14,22 +14,34 @@ public class Lab4P2_KevinBanegas {
         System.out.print("Nombre Jugador 2: ");
         String nombre2 = lea.nextLine();
         
-        Object [][] tablero = new Object [9][9]; //Colocar peones en el tablero
-        for (int i = 0; i < 9; i++) {
+        
+        
+        
+        Object [][] tablero1 = {{' ','A','B','C','D','E','F','G','H',}, //fila 0
+                                  {'1',new Torre(1, 1, 'r', false),new Caballo(1, 2, 'n', false),new Alfil(1, 3,  'b', false),new Dama(1, 4, 'q', false),new Rey(1, 5, 'k', false),new Alfil(1, 6,  'b', false),new Caballo(1, 7, 'n', false),new Torre(1, 8, 'r', false),},
+                                  {'2',' ',' ',' ',' ',' ',' ',' ',' ',},
+                                  {'3',' ',' ',' ',' ',' ',' ',' ',' ',},
+                                  {'4',' ',' ',' ',' ',' ',' ',' ',' ',},
+                                  {'5',' ',' ',' ',' ',' ',' ',' ',' ',},
+                                  {'6',' ',' ',' ',' ',' ',' ',' ',' ',},
+                                  {'7',' ',' ',' ',' ',' ',' ',' ',' ',},
+                                  {'8',new Torre(8, 1, 'R', true),new Caballo(8, 2, 'N', true),new Alfil(8, 3,  'B', true),new Dama(8, 4, 'Q', true),new Rey(8, 5, 'K', true),new Alfil(8, 6,  'B', true),new Caballo(8, 7, 'N', true),new Torre(8, 8, 'R', true),}};
+        Object [][] tablero = tablero1;
+        for (int i = 0; i < 9; i++) { //For para los peones
             for (int j = 0; j < 9; j++) {
-                if(i==2){
+                if(i==2 && j!=0){
                     tablero[i][j]=new Peon(i,j,'p',false);
-                }else if(i==7){
+                }else if(i==7 && j!=0){
                     tablero[i][j]= new Peon(i,j,'P',true);
                 } 
             }
         }
         System.out.println();
         System.out.println("TABLERO INICIAL: ");
-        System.out.println();
         imprimir(tablero);
+        
         boolean flag = true;
-        while(flag ==true){
+        while(flag ==true){ // while para las jugadas
             
         }
     }
@@ -40,8 +52,8 @@ public class Lab4P2_KevinBanegas {
                 System.out.print("[");
                 if(tablero[i][j] instanceof Piezas){
                     System.out.print(((Piezas)tablero[i][j]).getNombre());
-                }else{
-                    System.out.print(" ");
+                } else{
+                    System.out.print(tablero[i][j]);
                 }
                 System.out.print("] ");
             }

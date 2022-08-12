3d40441b91a47e0b38ff2afc8ea513b744d7ef47
unknown <jflg240104@unitec.edu>
2022-08-12 14:10:15 -0600

    avance

--- src/lab4p2_kevinbanegas/Lab4P2_KevinBanegas.java
+++ src/lab4p2_kevinbanegas/Lab4P2_KevinBanegas.java
@@ -1,10 +1,19 @@
 
 package lab4p2_kevinbanegas;
 
+import java.util.Scanner;
+
 public class Lab4P2_KevinBanegas {
 
+    static Scanner lea = new Scanner(System.in);
+    
     public static void main(String[] args) {
-        System.out.println("hola");
+        System.out.println();
+        System.out.print("Nombre Jugador 1: ");
+        String nombre1 = lea.nextLine();
+        System.out.println();
+        System.out.print("Nombre Jugador 2: ");
+        String nombre2 = lea.nextLine();
     }
 
 }

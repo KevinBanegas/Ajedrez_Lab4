c47e34f5c22b96e89e31c9b48c94cc24f768df93
KevinBanegas <kevinandre0504@gmail.com>
2022-08-12 16:32:45 -0600

    peones listos

--- src/lab4p2_kevinbanegas/Peon.java
+++ src/lab4p2_kevinbanegas/Peon.java
@@ -8,7 +8,7 @@ public class Peon extends Piezas {
     @Override
     public boolean validar(Object[][] tablero, int a, int b) {
         
-        if((a==x && b==y-1)&&tablero[a][b]==' '){
+        if((a==x && b==y-1)&&((char)(tablero[a][b]))==' '){
             return true;
         }else if(((a==x-1 || a==x+1) && b==y-1)&& tablero[a][b] instanceof Piezas){
             if(((Piezas)(tablero[a][b])).getColor()!=((Piezas)(tablero[x][y])).getColor()){

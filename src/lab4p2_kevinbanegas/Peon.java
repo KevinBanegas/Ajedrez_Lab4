042dc368f6c9a783b169a1b5125af3a0d1bb96d9
KevinBanegas <kevinandre0504@gmail.com>
2022-08-12 17:03:29 -0600

    peones listos

--- src/lab4p2_kevinbanegas/Peon.java
+++ src/lab4p2_kevinbanegas/Peon.java
@@ -5,17 +5,93 @@ public class Peon extends Piezas {
     public Peon(int x, int y, char nombre, boolean color) {
         super(x, y, nombre, color);
     }
+
     @Override
     public boolean validar(String mover, Object[][] tablero) {
-        
-//        if((x==a && y==b-1)&& ((tablero[a][b] instanceof Piezas)==false)){
-//            return true;
-//        }else if(((x==a-1 || x==a+1) && b==y-1)&& tablero[a][b] instanceof Piezas){
-//            if(((Piezas)(tablero[a][b])).getColor()!=((Piezas)(tablero[x][y])).getColor()){
-//                return true;
-//            }
-//        }
-        
+        String x11 = "";
+        String x22 = "";
+        x11 += mover.charAt(3);
+        int x1 = Integer.parseInt(x11);
+        x22 += mover.charAt(6);
+        int x2 = Integer.parseInt(x22);
+        int y1 = 0;
+        int y2 = 0;
+        switch (mover.charAt(2)) {
+            case 'a': {
+                y1 = 1;
+            }
+            break;
+            case 'b': {
+                y1 = 2;
+            }
+            break;
+            case 'c': {
+                y1 = 3;
+            }
+            break;
+            case 'd': {
+                y1 = 4;
+            }
+            break;
+            case 'e': {
+                y1 = 5;
+            }
+            break;
+            case 'f': {
+                y1 = 6;
+            }
+            break;
+            case 'g': {
+                y1 = 7;
+            }
+            break;
+            case 'h': {
+                y1 = 8;
+            }
+            break;
+        }
+        switch (mover.charAt(5)) {
+            case 'a': {
+                y2 = 1;
+            }
+            break;
+            case 'b': {
+                y2 = 2;
+            }
+            break;
+            case 'c': {
+                y2 = 3;
+            }
+            break;
+            case 'd': {
+                y2 = 4;
+            }
+            break;
+            case 'e': {
+                y2 = 5;
+            }
+            break;
+            case 'f': {
+                y2 = 6;
+            }
+            break;
+            case 'g': {
+                y2 = 7;
+            }
+            break;
+            case 'h': {
+                y2 = 8;
+            }
+            break;
+        }
+        if ((x1 == x2 && y1 == y2 - 1) && ((tablero[x1][y1] instanceof Piezas) == false)) {
+            return true;
+        } else if (((x1 == x2 - 1 || x1 == x2 + 1) && y1 == y2 - 1) && tablero[x1][y1] instanceof Piezas) {
+            if (((Piezas) (tablero[x1][y1])).getColor() != ((Piezas) (tablero[x1][y1])).getColor()) {
+                return true;
+            }
+        }
+
         return false;
     }
 }
